8f366c21198b9086d71167fd0222b04cd105fa10
unbun <uwh.547@gmail.com>
2020-07-13 19:50:06 -0400

    basic flight

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public Transform lookAt;
+    public float offset = 1.5f;
+    public float distance = 3.5f;
+    
+    private Vector3 desiredPos;
+
+    private void Start()
+    {
+        //Follow();
+    }
+
+    void LateUpdate()
+    {
+        Follow();
+    }
+
+    void Follow()
+    {
+        desiredPos = lookAt.position + (-transform.forward * distance) + (transform.up * offset); 
+        transform.position = Vector3.Lerp(transform.position, desiredPos, Time.deltaTime);
+        transform.LookAt(lookAt.position + Vector3.up * offset);
+    }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private CharacterController controller;
+
+    public float baseSpeed = 10.0f;
+    public float rotSpeedX = 3.0f;
+    public float rotSpeedY = 1.5f;
+    
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Vector3 move = transform.forward * baseSpeed;
+
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        Vector3 yaw = transform.right * (inputX * rotSpeedX * Time.deltaTime);
+        Vector3 pitch = transform.up * (inputY * rotSpeedY * Time.deltaTime);
+        Vector3 dir = yaw + pitch;
+
+        float maxX = Quaternion.LookRotation(move + dir).eulerAngles.x;
+        bool enteringLoop = (maxX < 90 && maxX > 70 || maxX > 270 && maxX < 290);
+        if (!enteringLoop)
+        {
+            move += dir;
+            transform.rotation = Quaternion.LookRotation(move);
+        }
+
+        controller.Move(move * Time.deltaTime);
+    }
+    
+}
