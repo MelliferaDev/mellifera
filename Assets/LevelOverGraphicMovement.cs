566b1a8d68bb1903dea1222c2166300c8bc1af89
Brock Fenbert <fenbert.b@husky.neu.edu>
2020-07-26 16:42:54 -0400

    working version of level over/ move on to next level

---
+++ Assets/LevelOverGraphicMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelOverGraphicMovement : MonoBehaviour
+{
+
+    public float moveDistance = 10f;
+    public float moveSpeed = 5f;
+    public float spinSpeed = 30f;
+
+    float maxY;
+    float minY;
+    bool up;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxY = transform.position.y + moveDistance;
+        minY = transform.position.y - moveDistance;
+        up = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float nextY;
+        if (up)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - moveSpeed * Time.deltaTime, transform.position.z);
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + moveSpeed * Time.deltaTime, transform.position.z);
+        }
+        if (transform.position.y < minY || transform.position.y > maxY) 
+        {
+            up = !up;
+            transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, minY, maxY), transform.position.z);
+        }
+        transform.Rotate(Vector3.up * Time.deltaTime * spinSpeed);
+    }
+}
---
+++ Assets/NextLevelCollision.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextLevelCollision : MonoBehaviour
+{
+
+    public GameObject nextLevelUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Time.timeScale = 0;
+            nextLevelUI.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+}
