04df167441df42309cb6d9550fcfa0c2ed393963
unbun <uwh.547@gmail.com>
2020-07-27 23:33:48 -0400

    forgot to commit this stuff

---
+++ Assets/Scripts/BetweenScenesCamera.cs
@@ -0,0 +1,36 @@
+﻿
+using UnityEngine;
+
+public class BetweenScenesCamera : MonoBehaviour
+{
+    [Range(0, 360)][SerializeField] private float angle;
+    [SerializeField] private float circleSpeed;
+    [SerializeField] private float distance;
+    [SerializeField] private float hoverDist;
+    [SerializeField] private float hoverSpeed;
+    
+    private Transform player;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        Debug.DrawLine(transform.position, player.position, Color.magenta);
+
+        angle += Time.deltaTime * circleSpeed;
+        angle %= 360;
+        
+        float targetX = distance * Mathf.Cos(Mathf.Deg2Rad * angle);
+        float targetY =  hoverDist * Mathf.Sin(Time.time * hoverSpeed);
+        float targetZ = distance * Mathf.Sin(Mathf.Deg2Rad * angle);
+
+        
+        Vector3 localPos = new Vector3(targetX, targetY, targetZ);
+        
+        transform.position = player.TransformPoint(localPos);
+        transform.LookAt(player);
+
+    }
+}
---
+++ Assets/Scripts/Player/PlayerUpgrades.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class PlayerUpgrades : MonoBehaviour
+    {
+        // static to maintain throughout each level
+        public static float maxSpeedAdd = 0.0f;
+        public static int maxHealthAdd = 0;
+        public static float attackMult = 1.0f;
+    }
+}
