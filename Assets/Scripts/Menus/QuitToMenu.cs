6c2765bd8c180d48545477ca13386e1552300e24
ccritter <chriscritter1@gmail.com>
2020-08-13 02:16:53 -0400

    Menu and Stats

---
+++ Assets/Scripts/Menus/QuitToMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QuitToMenu : MonoBehaviour
+{
+    public void ExitToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1f;
+    }
+}
